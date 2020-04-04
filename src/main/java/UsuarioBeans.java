688e91fcb7bc8b455e73b6ab0f3208cb5fb211e1
Fabio Gomes Rocha <gomesrocha@gmail.com>
2020-04-04 18:37:25 -0300

    Inicializando o projeto Web clinica médica, turma PP - Unit

---
+++ src/main/java/UsuarioBeans.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author fabio
+ */
+@ManagedBean
+@SessionScoped
+public class UsuarioBeans {
+
+    /**
+     * Creates a new instance of UsuarioBeans
+     */
+    private String login, senha, nome;
+    public UsuarioBeans() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String salvar(){
+        return "dashboard";
+    }
+    
+    
+}
