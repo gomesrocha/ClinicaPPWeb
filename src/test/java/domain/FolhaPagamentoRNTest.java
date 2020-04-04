6b911d0cd24e128e0446a95fc09151e7d06ee891
Fabio Gomes Rocha <gomesrocha@gmail.com>
2020-04-04 20:21:22 -0300

    Criand o teste da folha de pagamento

---
+++ src/test/java/domain/FolhaPagamentoRNTest.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fabio
+ */
+public class FolhaPagamentoRNTest {
+    
+    public FolhaPagamentoRNTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcularInss method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testCalcularInss() {
+        System.out.println("calcularInss");
+        FolhaPagamentoRN instance = null;
+        instance.calcularInss();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularIrrf method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testCalcularIrrf() {
+        System.out.println("calcularIrrf");
+        FolhaPagamentoRN instance = null;
+        instance.calcularIrrf();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularSalarioLiquido method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testCalcularSalarioLiquido() {
+        System.out.println("calcularSalarioLiquido");
+        FolhaPagamentoRN instance = null;
+        instance.calcularSalarioLiquido();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSalarioBruto method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testGetSalarioBruto() {
+        System.out.println("getSalarioBruto");
+        FolhaPagamentoRN instance = null;
+        double expResult = 0.0;
+        double result = instance.getSalarioBruto();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getInss method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testGetInss() {
+        System.out.println("getInss");
+        FolhaPagamentoRN instance = null;
+        double expResult = 0.0;
+        double result = instance.getInss();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getIrrf method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testGetIrrf() {
+        System.out.println("getIrrf");
+        FolhaPagamentoRN instance = null;
+        double expResult = 0.0;
+        double result = instance.getIrrf();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSalarioLiquido method, of class FolhaPagamentoRN.
+     */
+    @Test
+    public void testGetSalarioLiquido() {
+        System.out.println("getSalarioLiquido");
+        FolhaPagamentoRN instance = null;
+        double expResult = 0.0;
+        double result = instance.getSalarioLiquido();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
