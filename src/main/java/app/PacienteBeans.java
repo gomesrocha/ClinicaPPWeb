404709a7343f60943a5f4bdbf7924617aeb02959
Fabio Gomes Rocha <gomesrocha@gmail.com>
2020-06-03 19:53:08 -0300

    Atualizando aplicação

--- src/main/java/app/PacienteBeans.java
+++ src/main/java/app/PacienteBeans.java
@@ -7,12 +7,15 @@ package app;
 
 import infra.JPAUtils;
 import infra.Paciente;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -25,7 +28,9 @@ public class PacienteBeans {
     /**
      * Creates a new instance of PacienteBeans
      */
-    Paciente p = new Paciente();
+    private Paciente p = new Paciente();
+    private List<Paciente> pacientes = new ArrayList<Paciente>();
+    
     public PacienteBeans() {
     }
     public String salvarPaciente(){
@@ -37,9 +42,23 @@ public class PacienteBeans {
         et.commit();
         em.close();
         emf.close();
-        return "pacientes";
+        return "listPacientes";
     }
 
+    public List<Paciente> getPacientes() {   
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("puc");
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("select p from Paciente p", Paciente.class);
+        List<Paciente> ps = q.getResultList();
+        em.close();
+        emf.close();
+        return ps;
+    }
+
+    public void setPacientes(List<Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+    
     public Paciente getP() {
         return p;
     }
